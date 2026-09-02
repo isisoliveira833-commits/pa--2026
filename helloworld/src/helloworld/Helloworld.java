2592b8a47a72a5a9a324a3ebfccfc0283f8c6665
isis <isisoliveira833@gmqail.com>
2026-09-01 22:26:28 -0300

    Primeira aula de java

---
+++ helloworld/src/helloworld/Helloworld.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    { 
+  System.out.println("hello world");
+  System.out.println("joao lindo Sadades");
+  System.out.println(3*3);
+  
+  
+  //Entrada de valores
+  int Numero1 = 10;
+  int Numero2 = 20;
+  int resultado;
+  
+  //processamento
+  // Linha 30 e 31 Soma
+  resultado  =Numero1 + Numero2;
+  System.out.println("O Resultado da Soma é;" +resultado );
+
+  resultado = Numero1 / Numero2;
+ System.out.println("o resultado da divisao e´; + resultado");
+ 
+ resultado = Numero1 * Numero2;
+ System.out.println("o resultado da multiplicação é + resultado");
+
+ resultado = Numero1 - Numero2;
+ System.out.println(resultado);
+      
+ //saida de valores
+  
+  System.out.println(resultado);
+// TODO code application logic here
+    }
+    
+}
